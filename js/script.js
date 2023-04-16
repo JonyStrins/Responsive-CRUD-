d903a539544f4886bbd542cea00c9a175be76359
JonyStrins <slipknot1852@gmail.com>
2023-04-15 19:21:05 -0600

    Metodo Agregar Terminado

--- js/script.js
+++ js/script.js
@@ -20,13 +20,35 @@ function agregar() {
     const nombre = document.getElementById("txtFirstName").value;
     const puesto = document.getElementById("txtTitle").value;
     const tituloDeCortecia = document.getElementById("cboTitle").value;
+    const birthdate = document.getElementById("txtBirthdate").value;
+    const hiredate = document.getElementById("txtHireDate").value;
+    const address = document.getElementById("txtAddress").value;
+    const city = document.getElementById("txtCity").value;
+    const region = document.getElementById("cboRegion").value;
+    const cp = document.getElementById("txtCP").value;
+    const country = document.getElementById("txtCountry").value;
+    const ext = document.getElementById("txtExt").value;
+    const phone = document.getElementById("txtPhone").value;
+    const email = document.getElementById("txtEmail").value;
+    const reportsTo = document.getElementById("txtReportsTo").value;
     
     const newEmpleado = {
         id: empleados.length +1,
         lastName: lastName,
         firstName: nombre,
         title: puesto,
-        titleOfCourtesy: tituloDeCortecia
+        titleOfCourtesy: tituloDeCortecia,
+        birthdate: birthdate,
+        hiredate: hiredate,
+        address: address,
+        city: city,
+        region: region,
+        'postal code': cp,
+        country: country,
+        ext: ext,
+        phone: phone,
+        email: email,
+        reportsTo: reportsTo
     };
 
     empleados.push(newEmpleado);
